3bc8ab2e40f1158a2b1d2b2e72f1940fe2861894
bhuism <bhuism@odee.net>
2020-04-05 13:48:35 +0200

    add forkme

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Button, ButtonGroup, ButtonToolbar, Container, Jumbotron, Row} from "react-bootstrap";
+import ForkMe from "./components/ForkMe";
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+
+        <>
+            <ForkMe user={'bhuism'} repo={'citaten'}/>
+
+            <header>
+                <Jumbotron>
+                    <Container>
+                        <Row>
+                            <h1 className={'display-3'}>Citaten!</h1>
+                        </Row>
+                    </Container>
+                </Jumbotron>
+            </header>
+
+            <main>
+                <Container>
+                    <Row >
+                        <ButtonToolbar>
+                            <ButtonGroup className="mr-2">
+                                <Button variant={"primary"} size={"lg"} href={'#'}>Citaten »</Button>
+                            </ButtonGroup>
+                            <ButtonGroup className="mr-2">
+                                <Button variant={"primary"} size={"lg"} href={'#'}>Sprekers »</Button>
+                            </ButtonGroup>
+                            <ButtonGroup className="mr-2">
+                                <Button variant={"primary"} size={"lg"} href={'#'}>Categories »</Button>
+                            </ButtonGroup>
+                        </ButtonToolbar>
+                    </Row>
+                </Container>
+            </main>
+
+            <footer className="footer">
+                <div className="container">
+                    <span className="text-muted">©2019 <a className={'footerLink'}
+                                                          href="http://www.appsource.nl">Appsource</a>
+                    </span> - <a
+                    href="https://github.com/bhuism/citaten/commit/71a29129d2b43426344fba3398b824bbc7971a9a">
+                    <img alt="Latest badge"
+                         src="https://badge.odee.net/github/sha/bhuism/citaten/master/71a29129d2b43426344fba3398b824bbc7971a9a/badge.svg"/>
+                </a>
+                </div>
+            </footer>
+
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/ForkMe.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './ForkMe.scss'
+
+interface Props {
+    user: string,
+    repo: string
+}
+
+const ForkMe: React.FC<Props> = ({user, repo}) => (
+    <a className="github-fork-ribbon" href={'https://github.com/' + user + '/' + repo} data-ribbon="Fork me on GitHub"
+       title="Fork me on GitHub">Fork me on GitHub</a>
+);
+
+export default ForkMe;
+
+
+
