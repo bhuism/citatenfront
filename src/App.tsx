d2596d31d71929536356f20578baafe9b21f5f8b
bhuism <bhuism@odee.net>
2020-09-27 13:29:34 +0200

    add swagger-ui

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import {Button, ButtonGroup, ButtonToolbar, Container, Jumbotron, Row} from "rea
 import ForkMe from "./components/ForkMe";
 import Badge from "./components/Badge";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css'
+import './App.css';
+import "swagger-ui-react/swagger-ui.css";
+import SwaggerUI from "swagger-ui-react";
 
 function App() {
     return (
@@ -26,15 +28,21 @@ function App() {
                     <Row>
                         <ButtonToolbar>
                             <ButtonGroup className="mr-2">
-                                <Button variant={"primary"} size={"lg"} href={'https://api.citaten.odee.net/citaten'}>Citaten »</Button>
+                                <Button variant={"primary"} size={"lg"} href={'https://api.citaten.odee.net/citaten'}>Citaten
+                                    »</Button>
                             </ButtonGroup>
                             <ButtonGroup className="mr-2">
-                                <Button variant={"primary"} size={"lg"} href={'https://api.citaten.odee.net/sprekers'}>Sprekers »</Button>
+                                <Button variant={"primary"} size={"lg"} href={'https://api.citaten.odee.net/sprekers'}>Sprekers
+                                    »</Button>
                             </ButtonGroup>
                             <ButtonGroup className="mr-2">
-                                <Button variant={"primary"} size={"lg"} href={'https://api.citaten.odee.net/categorien'}>Categories »</Button>
+                                <Button variant={"primary"} size={"lg"}
+                                        href={'https://api.citaten.odee.net/categorien'}>Categories »</Button>
                             </ButtonGroup>
                         </ButtonToolbar>
+                    </Row>
+                    <Row>
+                        <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json"/>
                     </Row>
                 </Container>
             </main>
